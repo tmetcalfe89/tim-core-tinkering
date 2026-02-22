cab6b6c52ed4b82c868614ff62c4c88ed6456e14
tmetcalfe89 <tmetcalfe89@gmail.com>
2026-02-21 23:15:11 -0800

    removed unused module config

--- common/src/main/kotlin/us/timinc/mc/timcore/api/config/ModuleConfig.kt
+++
@@ -1,7 +0,0 @@
-package us.timinc.mc.timcore.api.config
-
-import us.timinc.mc.timcore.api.logging.Logger
-
-abstract class ModuleConfig {
-    val debugLevel: Logger.LogLevel = Logger.LogLevel.WARN
-}
